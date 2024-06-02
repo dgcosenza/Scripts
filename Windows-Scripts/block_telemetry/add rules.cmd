82cdba5adb18f31499afb489b129e5c164aa384b
Daniel G. Cosenza <inktrance@gmail.com>
2024-06-02 14:00:34 -0300

    Update add rules.cmd

--- Windows-Scripts/block_telemetry/add rules.cmd
+++ Windows-Scripts/block_telemetry/add rules.cmd
@@ -4,8 +4,9 @@
 :: and then runs a PowerShell script with elevated privileges to add
 :: the necessary firewall rules. After the rules are added, it displays
 :: a message indicating that the rules should now be included in the
-:: firewall configuration. Finally, it pauses to keep the window open
-:: for the user to see the output.
+:: firewall configuration. Then, it runs another PowerShell script
+:: to disable Windows telemetry services and tasks.
+:: Finally, it pauses to keep the window open for the user to see the output.
 :: --------------------------------------------------------------------------
 
 @echo off
@@ -13,7 +14,10 @@ cls
 echo Block Telemetry Firewall Rules 
 echo Confirm the UAC prompt to continue.
 echo.
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Scope -File ""%~dp0.\rules.ps1""' -Verb RunAs}"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0.\rules.ps1""' -Verb RunAs}"
 echo Rules should be included in Firewall.
+echo.
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0.\disable_telemetry.ps1""' -Verb RunAs}"
+echo The Windows Telemetry Services should now be Disabled.
 echo.
 pause
