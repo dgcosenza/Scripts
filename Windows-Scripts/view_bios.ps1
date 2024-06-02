ca9b891295429ee6c0c73ce00e6d624553a7f804
Daniel G. Cosenza <inktrance@gmail.com>
2024-06-02 01:43:57 -0300

    update

--- Windows-Scripts/view_bios.ps1
+++ Windows-Scripts/view_bios.ps1
@@ -1,1 +1,5 @@
+# --------------------------------------------------------------------------
+# This script retrieves information about the system BIOS using WMI (Windows Management Instrumentation).
+# --------------------------------------------------------------------------
+
 Get-WmiObject -Class Win32_BIOS
--- Windows-Scripts/view_bios_win_registry.ps1
+++ Windows-Scripts/view_bios_win_registry.ps1
@@ -1,1 +1,5 @@
+# --------------------------------------------------------------------------
+# This script retrieves registry information about the system BIOS.
+# --------------------------------------------------------------------------
+
 Get-ItemProperty -Path HKLM:\HARDWARE\DESCRIPTION\System\BIOS
