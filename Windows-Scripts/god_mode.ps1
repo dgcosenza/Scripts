0ecaeaa86755cf0086719733f55ceda8387ae46c
Daniel G. Cosenza <inktrance@gmail.com>
2024-06-02 14:12:17 -0300

    Create god_mode.ps1

---
+++ Windows-Scripts/god_mode.ps1
@@ -0,0 +1,5 @@
+# --------------------------------------------------------------------------
+# Create a folder named "GodMode" on the user's desktop with the special name that activates God Mode.
+# --------------------------------------------------------------------------
+
+New-Item -Path "$env:USERPROFILE\Desktop\GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}" -ItemType "directory"
