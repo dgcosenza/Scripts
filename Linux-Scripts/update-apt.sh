2db77c462b2409866db756cf81033ebfaff12b06
Daniel G. Cosenza <inktrance@gmail.com>
2024-06-02 02:50:37 -0300

    Update update-apt.sh

--- Linux-Scripts/update-apt.sh
+++ Linux-Scripts/update-apt.sh
@@ -1,9 +1,19 @@
+# --------------------------------------------------------------------------
+# This script performs a series of system package management tasks on a Debian-based system.
+# It updates the package lists, upgrades installed packages, removes unnecessary packages,
+# cleans package cache, and performs additional system cleanup using aptitude.
+# --------------------------------------------------------------------------
+
 clear
+
+# Update package lists and upgrade installed packages using apt-get
 sudo apt-get update -y &&
 sudo apt-get upgrade -y &&
 sudo apt-get autoremove -y &&
 sudo apt-get --purge autoremove -y &&
 sudo apt-get clean -y &&
+
+# Update package lists, upgrade installed packages, and perform cleanup using aptitude
 sudo aptitude update &&
 sudo aptitude upgrade &&
 sudo aptitude clean &&
