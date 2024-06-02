49f7a5d2106ffdf3f3ba9ac3ab3e46e4e345a6ae
Daniel G. Cosenza <inktrance@gmail.com>
2024-06-02 02:49:11 -0300

    Update password-generator.sh

--- Linux-Scripts/password-generator.sh
+++ Linux-Scripts/password-generator.sh
@@ -1,5 +1,15 @@
+# --------------------------------------------------------------------------
+# This script clears the terminal screen and generates a random password using pwgen.
+# The generated password includes special characters, numbers, and both uppercase and lowercase letters.
+# The length of the password is 21 characters.
+# --------------------------------------------------------------------------
+
 clear
+
+# Display a header for the password generator
 echo ""
 echo "Password Generator"
 echo ""
+
+# Generate a password using pwgen with specified options
 pwgen -s -y -B -n -C 21
