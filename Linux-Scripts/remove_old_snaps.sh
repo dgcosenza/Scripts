b4d1ce340ce061332d197a35ce379c2537cd281a
Daniel G. Cosenza <inktrance@gmail.com>
2024-06-02 02:52:33 -0300

    Update remove_old_snaps.sh

--- Linux-Scripts/remove_old_snaps.sh
+++ Linux-Scripts/remove_old_snaps.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# Removes old revisions of snaps
-# CLOSE ALL SNAPS BEFORE RUNNING THIS
+# --------------------------------------------------------------------------
+# This script removes old revisions of snaps.
+# CLOSE ALL SNAPS BEFORE RUNNING THIS.
+# It lists disabled snaps, extracts snap name and revision, then removes them.
+# --------------------------------------------------------------------------
 set -eu
 
 LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
