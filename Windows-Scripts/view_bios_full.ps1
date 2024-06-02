26845d64d3f1017f07a7861e34b743f870344611
Daniel G. Cosenza <inktrance@gmail.com>
2024-06-02 01:42:31 -0300

    Update view_bios_full.ps1

--- Windows-Scripts/view_bios_full.ps1
+++ Windows-Scripts/view_bios_full.ps1
@@ -1,1 +1,5 @@
+# --------------------------------------------------------------------------
+# This script retrieves detailed information about the system BIOS.
+# --------------------------------------------------------------------------
+
 Get-WmiObject -Class Win32_BIOS | Format-List *
